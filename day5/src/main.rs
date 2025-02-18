3deb7b68f18b858409d806cf6136fbc9b8180636
Nicolas Jaros <njaros@z3r5p6.42lyon.fr>
2025-02-18 14:45:38 +0100

    starting day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,27 @@
+use lib_aoc::input_lib;
+
+struct Segment {
+    value: [[u32; 2]; 2],
+}
+
+impl From<&str> for Segment {
+    fn from(s: &str) -> Self {
+        let mut seg = Segment{ value: [[0; 2]; 2]};
+        s.split(" -> ").enumerate().for_each(|(idx1, pair)| {
+            pair.split(',').enumerate().for_each(|(idx2, n_str)| {
+                seg.value[idx1][idx2] = n_str.parse().unwrap();
+            });
+        });
+        seg
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut content = input_lib::get_input_as_string(file!(), true);
+    let segments: Vec<Segment> = Vec::new();
+    content.split('\n').for_each(|line| {
+        let new_segment: Segment = line.into();
+    });
 }
