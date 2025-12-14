418e6cde7f22a760c7fa517b701d6bca2909186a
Nicolas Jaros <njaros@z3r6p4.42lyon.fr>
2025-12-14 14:14:00 +0100

    day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,34 +1,73 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
+use itertools::Itertools;
+use lib_aoc::{input_lib};
 
-use lib_aoc::{input_lib, segment::{InterNoDiagonal, Segment}};
+fn lerp(s: ((i32, i32), (i32, i32))) -> Vec<(i32, i32)> {
+    let mut points = Vec::<(i32, i32)>::new();
+    let mut p = s.0;
+    let step_x;
+    let step_y;
 
-// A lot of useless implementations, that's to learn the language.
-
-fn main() {
-    let content = input_lib::get_input_as_string(file!(), false);
-    let mut cross_set: BTreeSet<[i64; 2]> = BTreeSet::new();
-
-    // Collect all segments in a vector.
-    let mut segments: Vec<Segment> = Vec::new();
-    content.split('\n').for_each(|line| {
-        segments.push(line.into());
-    });
-
-    // Only retain horizontal or vertical segments.
-    segments.retain(|s| s.value[0][0] == s.value[1][0] || s.value[0][1] == s.value[1][1]);
-
-    // debug part
-    for i in 0..segments.len() {
-        for j in i + 1 .. segments.len() {
-            for point in segments[i].inter_points_nd(&segments[j]) {
-                cross_set.insert(point);
-            }
-        }
+    if s.0.0 < s.1.0 {
+        step_x = 1;
+    }
+    else if s.0.0 > s.1.0 {
+        step_x = -1;
+    }
+    else {
+        step_x = 0;
     }
 
-    // for point in &cross_set {
-    //     println!("{}, {}\n", point[0], point[1]);
-    // }
+    if s.0.1 < s.1.1 {
+        step_y = 1;
+    }
+    else if s.0.1 > s.1.1 {
+        step_y = -1;
+    }
+    else {
+        step_y = 0;
+    }
 
-    println!("Result is {}", cross_set.len())
+    while p != s.1 {
+        points.push(p);
+        p.0 += step_x;
+        p.1 += step_y;
+    }
+    points.push(p);
+    points
+}
+
+fn main() {
+    let part = input_lib::get_part();
+    let input = input_lib::get_input_as_string(file!(), false);
+    
+    let segments = input
+    .split("\n")
+    .map(|line| {
+        let (p1_raw, p2_raw) = line.split_once(" -> ").unwrap();
+        let (x1_raw, y1_raw) = p1_raw.split_once(',').unwrap();
+        let (x2_raw, y2_raw) = p2_raw.split_once(',').unwrap();
+        ((x1_raw.parse::<i32>().unwrap(), y1_raw.parse::<i32>().unwrap()), ((x2_raw.parse::<i32>().unwrap()), (y2_raw.parse::<i32>().unwrap())))
+    })
+    .collect_vec();
+
+    println!("Result is {}",
+        segments
+        .iter()
+        .filter(|segment| match part {
+            1 => segment.0.0 == segment.1.0 || segment.0.1 == segment.1.1,
+            _ => true
+        })
+        .fold((HashSet::<(i32, i32)>::new(), HashSet::<(i32, i32)>::new()), |mut acc, &segment| {
+            lerp(segment)
+            .iter()
+            .for_each(|&p| {
+                if !acc.0.insert(p) {
+                    acc.1.insert(p);
+                }
+            });
+            acc
+        })
+        .1.len()
+    )
 }
--- lib_aoc/src/lib.rs
+++ lib_aoc/src/lib.rs
@@ -74,6 +74,9 @@ pub mod input_lib {
     }
 }
 
+/**
+ * Some algebric tools
+ */
 pub mod math {
 
     /// Return the maximum between a Scalar A and a scalar B.
@@ -90,6 +93,14 @@ pub mod math {
             return a;
         }
         b
+    }
+
+    /// Reorder two comparable values
+    pub fn reorder_pair<T: std::cmp::PartialOrd>(a: T, b: T) -> (T, T) {
+        if a <= b {
+            return (a, b);
+        }
+        (b, a)
     }
 
     pub fn sub_abs<T: std::cmp::PartialOrd + std::ops::Sub + std::ops::Sub<Output = T>>(a: T, b: T) -> T {        
@@ -130,11 +141,11 @@ pub mod math {
  * Tools for lines in usual vectorial spaces
  */
 pub mod lines {
+    use crate::math::reorder_pair;
+
 
     // This variable may need to be higher, for now I don't know.
     pub const ESPILON: f64 = f64::EPSILON;
-
-    pub type Point<Dim> = [Dim; 2];
 
     #[derive(Debug, Clone, Copy)]
     pub struct Point2d {
@@ -142,6 +153,7 @@ pub mod lines {
         pub y: f64
     }
 
+    #[derive(Debug, Clone, Copy)]
     pub struct Segment2d {
         pub p1: Point2d,
         pub p2: Point2d,
@@ -241,39 +253,134 @@ pub mod lines {
 
     // Traits/impl for Segment
 
-    trait LineDir<> {
+    trait LineDir {
 
-        fn get_line_coefficients(&self, p1: Point2d, p2: Point2d) -> (f64, f64);
-        fn get_leading_coefficient(&self, p1: Point2d, p2: Point2d) -> f64;
-        fn get_y_intercept(&self, a: f64, p: Point2d) -> f64;
+        fn get_line_coefficients(p1: Point2d, p2: Point2d) -> (f64, f64);
+        fn get_leading_coefficient(p1: Point2d, p2: Point2d) -> f64;
+        fn get_y_intercept(a: f64, p: Point2d) -> f64;
+        
+    }
+
+    pub trait GetPoint {
+
+        fn get_y_from_x(&self, x: f64) -> f64;
+        fn get_x_from_y(&self, y: f64) -> f64;
+        fn get_point_from_x(&self, x: f64) -> Point2d;
+        fn get_point_from_y(&self, y: f64) -> Point2d;
+        fn contain_point(&self, p: Point2d) -> bool;
 
     }
 
     pub trait Intercept<L, P> {
 
+        /**
+         * Get the interception point if exists of the infinite
+           lines where the segments belong to.
+         */
+        fn intercept_point_lines(&self, other: &L) -> Option<P>;
+
+        /**
+         * Get the interception point of 2 segments if exists.
+         */
         fn intercept_point(&self, other: &L) -> Option<P>;
-        
+
+        /**
+         * Return true if the segment intercepts with the other.
+         */
+        fn intercept(&self, other: &L) -> bool;
+
     }
 
     impl LineDir for Segment2d {
 
-        fn get_leading_coefficient(&self, p1: Point2d, p2: Point2d) -> f64 {
-            (p1.x - p2.x) / (p1.y - p2.y)
+        fn get_leading_coefficient(p1: Point2d, p2: Point2d) -> f64 {
+            (p1.y - p2.y) / (p1.x - p2.x)
         }
 
-        fn get_y_intercept(&self, a: f64, p: Point2d) -> f64 {
+        fn get_y_intercept(a: f64, p: Point2d) -> f64 {
             p.y - a * p.x
         }
 
-        fn get_line_coefficients(&self, p1: Point2d, p2: Point2d) -> (f64, f64) {
-            let a = self.get_leading_coefficient(p1, p2);
-            (a, self.get_y_intercept(a, p1))
+        fn get_line_coefficients(p1: Point2d, p2: Point2d) -> (f64, f64) {
+            let a = Self::get_leading_coefficient(p1, p2);
+            (a, Self::get_y_intercept(a, p1))
         }
+
+    }
+
+    impl GetPoint for Segment2d {
+
+        fn get_y_from_x(&self, x: f64) -> f64 {
+            self.a * x + self.b
+        }
+
+        fn get_x_from_y(&self, y: f64) -> f64 {
+            (y - self.b) / self.a
+        }
+
+        fn get_point_from_x(&self, x: f64) -> Point2d {
+            Point2d { x, y: self.get_y_from_x(x) }
+        }
+
+        fn get_point_from_y(&self, y: f64) -> Point2d {
+            Point2d { x: self.get_x_from_y(y), y }
+        }
+
+        fn contain_point(&self, p: Point2d) -> bool {
+            let (min_x, max_x) = reorder_pair(self.p1.x, self.p2.x);
+            let (min_y, max_y) = reorder_pair(self.p1.y, self.p2.y);
+            match (p.x < min_x) || (p.x > max_x) || (p.y < min_y) || (p.y > max_y) {
+                true => false,
+                _ => {
+                    super::math::sub_abs(self.get_y_from_x(p.x), p.y) < ESPILON
+                }
+            }
+        }
+
+    }
+
+    impl Intercept<Segment2d, Point2d> for Segment2d {
+
+        fn intercept_point_lines(&self, other: &Segment2d) -> Option<Point2d> {
+            match self.a == other.a {
+                // parallel
+                true => None,
+                // not parallel
+                _ => Some(self.get_point_from_x((other.b - self.b) / (self.a - other.a)))
+            }
+        }
+
+        fn intercept_point(&self, other: &Segment2d) -> Option<Point2d> {
+            let (min_x, max_x) = reorder_pair(self.p1.x, self.p2.x);
+            let (min_y, max_y) = reorder_pair(self.p1.y, self.p2.y);
+            let (min_x_other, max_x_other) = reorder_pair(other.p1.x, other.p2.x);
+            let (min_y_other, max_y_other) = reorder_pair(other.p1.y, other.p2.y);
+
+            match (min_x_other > max_x) || (max_x_other < min_x) || (min_y_other > max_y) || (max_y_other < min_y) {
+                true => None,
+                _ => {
+                    match self.intercept_point_lines(other) {
+                        None => None,
+                        Some(p) => {
+                            match p.x < min_x || p.x > max_x || p.y < min_y || p.y > max_y {
+                                true => None,
+                                _ => Some(p)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        fn intercept(&self, other: &Segment2d) -> bool {
+            self.intercept_point(other).is_some()
+        }
+
     }
 
     impl Segment2d {
-        pub fn new(&self, pt1: Point2d, pt2: Point2d) -> Self {
-            let (a_, b_) = self.get_line_coefficients(pt1, pt2);
+        pub fn new(pt1: Point2d, pt2: Point2d) -> Self {
+            let (a_, b_) = Self::get_line_coefficients(pt1, pt2);
             Self {p1: pt1, p2: pt2, a: a_, b: b_}
         }
     }
